9ed5526702c2fcbef8ba93240274bac72699dc4f
sergei.dubovik <sergei.dubovik@thomsonreuters.com>
2017-05-10 15:05:16 +0300

    Web rest with auth was added.

---
+++ IM.EasyDev.WebApi/IM.EasyDev.Web.Rest/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+
+namespace IM.EasyDev.Web.Rest.Extensions
+{
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures a provided <see cref="T:System.Web.Http.HttpConfiguration" /> instance using a provided action.
+        /// </summary>
+        /// <param name="httpConfig">A <see cref="T:System.Web.Http.HttpConfiguration" /> instance to configure.</param>
+        /// <param name="cfgAction">An action, which configures a provided <see cref="T:System.Web.Http.HttpConfiguration" /> instance.</param>
+        /// <returns>A configured <see cref="T:System.Web.Http.HttpConfiguration" /> instance.</returns>
+        public static HttpConfiguration ConfigWith(
+            this HttpConfiguration httpConfig, Action<HttpConfiguration> cfgAction)
+        {
+            if (cfgAction == null)
+            {
+                throw new ArgumentNullException(nameof(cfgAction));
+            }
+
+            cfgAction(httpConfig);
+            return httpConfig;
+        }
+
+        /// <summary>
+        /// Populates the <see cref="P:System.Web.Http.HttpConfiguration.DependencyResolver" /> property for a provided <see cref="T:System.Web.Http.HttpConfiguration" /> object using a provided action.
+        /// </summary>
+        /// <param name="httpConfig">A <see cref="T:System.Web.Http.HttpConfiguration" /> instance to set a dependency resolver for.</param>
+        /// <param name="cfgAction">An action, which returns a dependency resolver for a provided <see cref="T:System.Web.Http.HttpConfiguration" /> instance.</param>
+        /// <returns>A <see cref="T:System.Web.Http.HttpConfiguration" /> instance with dependency resolver provided by a <paramref name="cfgAction"/> action.</returns>
+        public static HttpConfiguration ConfigWithDependencyResolver(
+            this HttpConfiguration httpConfig, Func<IDependencyResolver> cfgAction)
+        {
+            if (cfgAction == null)
+            {
+                throw new ArgumentNullException(nameof(cfgAction));
+            }
+
+            httpConfig.DependencyResolver = cfgAction();
+            return httpConfig;
+        }
+    }
+}
---
+++ IM.EasyDev.WebApi/IM.EasyDev.Web.Rest/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using IM.EasyDev.Web.Rest.Extensions;
+
+[assembly: OwinStartup(typeof(IM.EasyDev.Web.Rest.Startup))]
+
+namespace IM.EasyDev.Web.Rest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.ConfigWith(WebApiConfig.Register);
+            ConfigureAuth(app);
+            app.UseWebApi(config);    
+        }
+    }
+}
